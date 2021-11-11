473b99526a222148eb615347d5f65d7ffe60ac17
zhulu <zhululu@weihaizixun.com>
2021-11-11 16:55:17 +0800

    支持集成添加

--- custom_components/xiaomi_tv/config_flow.py
+++ custom_components/xiaomi_tv/config_flow.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import Any
+import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.data_entry_flow import FlowResult
@@ -19,10 +20,7 @@ class SimpleConfigFlow(ConfigFlow, domain=DOMAIN):
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
-        """Handle the initial step."""
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
-
+    
         if user_input is None:
             errors = {}
             return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
