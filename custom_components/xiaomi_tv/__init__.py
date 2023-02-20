646fcbbde4db9d92625ee668dd78a01487179a94
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2023-02-20 22:46:54 +0800

    删除语音控制

--- custom_components/xiaomi_tv/__init__.py
+++ custom_components/xiaomi_tv/__init__.py
@@ -3,16 +3,12 @@ from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 import threading
 from .const import DOMAIN, PLATFORMS
-from .intent_script import async_register
 
 CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(update_listener))
-    # 延时注册
-    timer = threading.Timer(3, async_register, [hass])
-    timer.start()
     return True
 
 async def update_listener(hass, entry):
