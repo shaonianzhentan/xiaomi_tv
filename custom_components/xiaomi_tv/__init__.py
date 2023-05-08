e304d256266efef47429015915636d35381f2b0a
shaonianzhentan <ilovevar@gmail.com>
2023-05-08 17:58:31 +0800

    update

--- custom_components/xiaomi_tv/__init__.py
+++ custom_components/xiaomi_tv/__init__.py
@@ -1,18 +1,13 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
-import threading
 from .const import DOMAIN, PLATFORMS
 
 CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(update_listener))
+    hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
-
-async def update_listener(hass, entry):
-    config = entry.options
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
